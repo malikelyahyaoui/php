974cc5f4aba80b9cac02e7ba60b5f35f08e41aed
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-09-25 15:32:42 +0200

    Update exo5.php

--- exo5.php
+++ exo5.php
@@ -10,22 +10,17 @@ Attention, la valeur générée devra être arrondire à 2 décimales</p>
 <h2>Résultat</h2>
 
 <?php
-// Déclaration du taux de conversion fixe
 define("TAUX_CONVERSION", 6.55957);
 
-// Fonction pour convertir des francs en euros
 function convertirFrancsEnEuros($valeurFrancs) {
-    // Conversion
     $valeurEuros = $valeurFrancs / TAUX_CONVERSION;
 
-    // Arrondi à 2 décimales
     $valeurEurosArrondie = round($valeurEuros, 2);
 
     return $valeurEurosArrondie;
 }
 
-// Exemple d'utilisation
-$valeurFrancs = 200; // valeur en francs
+$valeurFrancs = 200; 
 $valeurEuros = convertirFrancsEnEuros($valeurFrancs);
 
 echo "$valeurFrancs francs = $valeurEuros euros";
