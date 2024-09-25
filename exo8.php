93ce50dd32d53ec4414f3390d55a98cb23209849
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-09-25 15:33:24 +0200

    Update exo8.php

--- exo8.php
+++ exo8.php
@@ -6,18 +6,13 @@ Ecrire un algorithme qui renvoie la table de multiplication d'un nombre passé e
 <h2>Résultat</h2>
 
 <?php
-// Fonction pour afficher la table de multiplication d'un nombre
 function tableMultiplication($nombre) {
-    // Boucle de 1 à 10 pour afficher la table de multiplication
     for ($i = 1; $i <= 10; $i++) {
-        // Calcul du produit
         $produit = $nombre * $i;
         
-        // Affichage du résultat sous forme "nombre x i = produit"
         echo "$nombre x $i = $produit <br>";
     }
 }
 
-// Exemple d'utilisation
-$nombre = 7; // Le nombre dont on veut afficher la table
+$nombre = 7; 
 tableMultiplication($nombre);
